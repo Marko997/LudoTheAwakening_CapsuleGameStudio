c251d12da09d2e14d48546f736682696bceb548b
Marko Simijonovic <msimijonovic97@gmail.com>
2020-11-26 15:52:44 +0000

    Added: - Loading screen Changes: - Changed build settings to android

---
+++ Assets/Scripts/LevelLoaderManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoaderManager : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public string sceneToLoad;
+    AsyncOperation loadingOperation;
+    public Slider slider;
+    public Text percentLoaded;
+
+    void Start() 
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+
+    void Update()
+    {
+        float progressValue = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+        slider.value = progressValue;
+        percentLoaded.text = Mathf.Round(progressValue * 100) + "%";
+    }
+
+
+    // public void LoadLevel(int sceneIndex){
+
+    //     StartCoroutine(LoadAsync(sceneIndex));
+        
+
+    // }
+
+    // IEnumerator LoadAsync (int sceneIndex){
+
+    //     AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+    //     loadingScreen.SetActive(True);
+
+    //     while(operation.isDone==false){
+
+    //         float progress = Mathf.Clamp01(operation.progress / .9f);
+
+    //         Debug.Log(progress);
+
+    //         yield return null;
+    //     }
+    // }
+}
