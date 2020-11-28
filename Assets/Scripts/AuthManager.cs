7c2fca4959676bcddda3b048403e30731fbdc9d8
Aleksa Arandjelovic <aleksa.arandjelovic92@gmail.com>
2020-11-28 15:14:17 +0000

    Auth manager pocetak

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+public class AuthManager : MonoBehaviour
+{
+    //Firebase Variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+
+    //Login Variables
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+//Register variables
+    [Header("Register")]
+    public TMP_InputField userNameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
+
+}
