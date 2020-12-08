a6c9fa54c56c3cc450a3b2c8f018782aab386e5d
Marko Simijonovic <msimijonovic97@gmail.com>
2020-12-08 21:28:55 +0000

    Added: - New logic for player management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static GameManager instance;
+
+    public List<Entity> playerList = new List<Entity>();
+
+    public enum States{
+        WAITING,
+        ROLL_DICE,
+        SWITCH_PLAYER
+    }
+
+    public States state;
+
+    public int activePlayer;
+
+    bool switchingPlayer;
+
+    void Awake() {
+        instance = this;
+    }
+
+    void Update() {
+
+        if(playerList[activePlayer].playerTypes == Entity.PlayerTypes.BOT){
+            switch(state){
+            case States.ROLL_DICE:
+                StartCoroutine(RollDiceDelay());
+                state = States.WAITING;
+            break;
+            case States.WAITING:
+
+            break;
+            case States.SWITCH_PLAYER:
+
+            break;
+            } 
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void RollDice(){
+        int diceNumber = Random.Range(1,7);
+
+        if(diceNumber == 6){
+            //check start node
+
+        }
+        if(diceNumber <6){
+            //check for kick
+        }
+        Debug.Log(diceNumber+"sadsadsadsa");
     }
+
+    IEnumerator RollDiceDelay(){
+        yield return new WaitForSeconds(2);
+        RollDice();
+    }
+    
+
 }
