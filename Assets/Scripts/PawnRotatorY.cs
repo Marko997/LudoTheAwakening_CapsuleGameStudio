0596a41d579db24f55f70ea52cb2642011604d23
Marko Simijonovic <msimijonovic97@gmail.com>
2020-12-21 21:16:14 +0000

    Added: - New menu scene with character in main focus - not finished - You can change your pawn in scene, but not ingame

---
+++ Assets/Scripts/PawnRotatorY.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PawnRotatorY : MonoBehaviour, IPointerUpHandler,IPointerDownHandler
+{
+    bool isPressed;
+    bool isMoving;
+
+    Touch touch;
+
+    [SerializeField]
+    Transform pawnHolder;
+
+    [SerializeField]
+    GameObject pawnSelectionMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed){
+            if(Input.touchCount > 0){
+
+                touch = Input.GetTouch(0);
+
+                if(touch.phase == TouchPhase.Moved){
+
+                        pawnHolder.Rotate(0,-touch.deltaPosition.x,0);
+                    
+                    isMoving = true;
+                }
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if(isMoving == false){
+            pawnSelectionMenu.SetActive(true);
+        }
+        isPressed = false;
+        isMoving = false;
+    }
+}
---
+++ Assets/Scripts/SelectPawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] pawns;
+
+    int pawnIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangePawn(){
+        for(int i=0; i < pawns.Length; i++){
+            
+            if( i == pawnIndex ){
+            
+                pawns[pawnIndex].gameObject.SetActive(true);
+                
+            }else{
+
+                pawns[i].gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    public void SelectBlueSanta(){
+        pawnIndex = 1;
+        ChangePawn();
+    }
+    public void SelectRedSanta(){
+        pawnIndex = 0;
+        ChangePawn();
+    }
+}
