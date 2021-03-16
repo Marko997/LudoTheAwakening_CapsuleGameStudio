bc9142e0b8673f6137b9aa3e6611269a615ce12b
Marko Simijonovic <msimijonovic97@gmail.com>
2021-03-16 21:53:03 +0000

    Added: - Complete UI placeholders for main menu - Some UI components for others scenes

---
+++ Assets/Scripts/Spells/Spell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public NodeManager goalNode;
+    public int eatPower;
+
+    public Spell (string name, NodeManager goalNode,int eatPower)
+	{
+        this.name = name;
+        this.goalNode = goalNode;
+        this.eatPower = eatPower;
+
+	}
+
+    public void Cast()
+	{
+        Debug.Log("Cast spell");
+        //CHECK POSSIBLE KICK
+
+        if (goalNode.isTaken)
+        {
+            //KICK THE OTHER STONE
+            goalNode.pawn.ReturnToBase();
+        }
+    }
+}
