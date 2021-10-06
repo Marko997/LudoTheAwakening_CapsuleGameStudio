623ff7c29e34c05b69ab8d432f88f0fd66d6b8ea
Marko Simijonovic <mrankovic.it@gmail.com>
2021-10-06 21:46:34 +0000

    Added: - PlayFab Login and Register with email and username

--- Assets/Scripts/Authentification/PlayFabUserLogin.cs
+++ Assets/Scripts/Authentification/PlayFabUserLogin.cs
@@ -2,50 +2,107 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
+using System;
 
 public class PlayFabUserLogin : MonoBehaviour
 {
-    [SerializeField] private GameObject signInDisplay = default;
+    public bool ClearPlayerPrefs;
+
     [SerializeField] private TMP_InputField usernameInputField = default;
     [SerializeField] private TMP_InputField emailInputField = default;
     [SerializeField] private TMP_InputField passwordInputField = default;
+    [SerializeField] private TMP_InputField confirmPasswordInputField = default;
 
-    public static string SessionTicket;
+    public Button loginButton;
+    public Button playAsGuestButton;
+    public Button loginWithFacebookButton;
+    public Button loginWithGoogleButton;
+    public Button registerButton;
+    public Button cancelRegisterButton;
 
-    public void CreateAccount()
+    [SerializeField] GameObject registerPanel, loginPanel, startPanel;
+
+    string encryptedPassword;
+    public void SwitchToSignUpTab()
     {
-        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(true);
+    }
+
+    public void SwitchLoginTab()
+    {
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+    }
+
+    string Encrypt (string pass)
+    {
+        System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] bs = System.Text.Encoding.UTF8.GetBytes(pass);
+        bs = x.ComputeHash(bs);
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+
+        foreach (byte b in bs)
         {
-            Username = usernameInputField.text,
+            s.Append(b.ToString("x2").ToLower());
+        }
+
+        return s.ToString();
+    }
+
+    public void SignUp()
+    {
+        var registerRequest = new RegisterPlayFabUserRequest
+        {
             Email = emailInputField.text,
-            Password = passwordInputField.text
-        }, result =>
-        {
-            SessionTicket = result.SessionTicket;
-            signInDisplay.SetActive(false);
-        }, error =>
-         {
-             Debug.LogError(error.GenerateErrorReport());
-         });
+            Username = "smt",
+            Password = Encrypt(passwordInputField.text),
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, RegisterSuccess, RegisterError);
     }
 
-    public void SignIn()
+    public void RegisterSuccess(RegisterPlayFabUserResult result)
     {
-        //With username
-        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
-        {
-            Username = usernameInputField.text,
-            Password = passwordInputField.text
-        }, result =>
-        {
-            SessionTicket = result.SessionTicket;
-            signInDisplay.SetActive(false);
-        }, error =>
-        {
-            Debug.LogError(error.GenerateErrorReport());
-        });
+        //errorSignUp.text = "";
+        Debug.Log("suc");
+        StartGame();
     }
+
+    public void RegisterError(PlayFabError error)
+    {
+        //errorSignUp.text = error.GenerateErrorReport(); 
+        Debug.Log(error.GenerateErrorReport());
+    }
+    public void LoginUser() {
+        
+        var request = new LoginWithEmailAddressRequest {
+            Email = emailInputField.text,
+            Password = Encrypt(passwordInputField.text)
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, LoginSuccess, LoginError);
+    }
+    public void LoginSuccess(LoginResult result)
+    {
+        //errorLogin.text = "";
+        Debug.Log("suc");
+        StartGame();
+    }
+    public void LoginError(PlayFabError error)
+    {
+        //errorLogin.text = error.GenerateErrorReport(); 
+        Debug.Log(error.GenerateErrorReport());
+    }
+    private void StartGame()
+    {
+        startPanel.SetActive(false);
+
+    }
+
+
+
+
 
 }
