13fab2fdbeb69045141f1b6c479f7618f21336b1
MilosJovanovicRT-5218 <54739550+MilosJovanovicRT-5218@users.noreply.github.com>
2022-11-15 13:59:56 +0100

    Added:Login

--- Assets/Scripts/Login/DontDestroy.cs
+++ Assets/Scripts/Login/DontDestroy.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DontDestroy : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+
+    //Brise objekat Ime
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // here you can use scene.buildIndex or scene.name to check which scene was loaded
+        if (scene.name == "Game")
+        {
+            // Destroy the gameobject this script is attached to
+            Destroy(gameObject);
+        }
     }
 }
