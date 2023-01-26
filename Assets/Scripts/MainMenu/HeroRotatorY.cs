211cc98ec2a31e9eacfd960a8ca2968a010ac5f7
Marko997 <mrankovic.it@gmail.com>
2023-01-26 12:36:12 +0100

    Fixed: hero rotation on main screen

--- Assets/Scripts/MainMenu/HeroRotatorY.cs
+++ Assets/Scripts/MainMenu/HeroRotatorY.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HeroRotatorY : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+public class HeroRotatorY : MonoBehaviour//, IPointerUpHandler, IPointerDownHandler
 {
     bool isPressed;
     bool isMoving;
+
+    public float rotationSpeed = 90f;
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    //    }
+    //}
 
     Touch touch;
 
@@ -15,37 +25,56 @@ public class HeroRotatorY : MonoBehaviour, IPointerUpHandler, IPointerDownHandle
 
     void Update()
     {
-        if (isPressed)
+        if (Input.GetMouseButton(0))
         {
-            if (Input.touchCount > 0)
+            pawnHolder.Rotate(0, -Input.GetAxis("Mouse X") * rotationSpeed, 0);
+            isMoving = true;
+        }else if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved || Input.GetMouseButton(0))
             {
 
-                touch = Input.GetTouch(0);
-
-                if (touch.phase == TouchPhase.Moved)
-                {
-
-                    pawnHolder.Rotate(0, -touch.deltaPosition.x, 0);
+                pawnHolder.Rotate(0, -touch.deltaPosition.x, 0);
 
 
-                    isMoving = true;
-                }
+                isMoving = true;
             }
         }
+
+
+        //if (isPressed)
+        //{
+        //    if (Input.touchCount > 0 || Input.GetMouseButton(0))
+        //    {
+
+        //        touch = Input.GetTouch(0);
+
+        //        if (touch.phase == TouchPhase.Moved || Input.GetMouseButton(0))
+        //        {
+
+        //            pawnHolder.Rotate(0, -touch.deltaPosition.x, 0);
+
+
+        //            isMoving = true;
+        //        }
+        //    }
+        //}
     }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isPressed = true;
-    }
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    isPressed = true;
+    //}
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (isMoving == false)
-        {
-            //pawnSelectionMenu.SetActive(true);
-        }
-        isPressed = false;
-        isMoving = false;
-    }
+    //public void OnPointerUp(PointerEventData eventData)
+    //{
+    //    if (isMoving == false)
+    //    {
+    //        //pawnSelectionMenu.SetActive(true);
+    //    }
+    //    isPressed = false;
+    //    isMoving = false;
+    //}
 }
