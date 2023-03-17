9469a21302899559169a83a10ac4ab487027aefe
Marko997 <mrankovic.it@gmail.com>
2023-03-17 19:06:15 +0100

    Added: Finished fully working chat system

--- Assets/Scripts/Net/ChatSystem.cs
+++ Assets/Scripts/Net/ChatSystem.cs
@@ -13,49 +13,82 @@ public class ChatSystem : NetworkBehaviour
 
 
     bool isChatOppened;
+    bool isTurningOffRunning = false;
 
     NetworkVariable<int> chatEmojiIndex = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    NetworkVariable<int> chatEmojiIndex1 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    NetworkVariable<int> chatEmojiIndex2 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    NetworkVariable<int> chatEmojiIndex3 = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
+    [SerializeField] private Sprite[] emojis;
+    [SerializeField] private Image[] emojisInChatMenu;
 
+    private Coroutine timerCoroutine;
 
-    // Start is called before the first frame update
     void Start()
     {
         RegisterEvents();
 
-        //rotiranje red playera sa playerom ciji je screen
-        if(NetworkManager.Singleton.LocalClientId > 0)
-        {
-            var temp = allChatDisplayIcons[0];
-            allChatDisplayIcons[0] = allChatDisplayIcons[NetworkManager.Singleton.LocalClientId];
-            allChatDisplayIcons[NetworkManager.Singleton.LocalClientId] = temp;
-        }
-
         chatEmojiDisplay.SetActive(false);
         chatButton.onClick.AddListener(OnChatButtonPressed);
 
-        foreach (Transform item in chatEmojiDisplay.transform)
+        for (int i = 0; i < 4; i++)
         {
-            item.gameObject.GetComponentInChildren<Button>().onClick.AddListener(ShowEmojiToOtherPlayersServerRpc);
+            emojisInChatMenu[i].sprite = emojis[i];
         }
+            switch (NetworkManager.Singleton.LocalClientId)
+            {
+                case 0:
+                int childCount = chatEmojiDisplay.transform.childCount;
 
-        //chatEmojiDisplay.GetComponentInChildren<Button>().onClick.AddListener(ShowEmojiToOtherPlayers);
-    }
+                //for (int i = 0; i < 4; i++) //UNITY BUG ALWAYS SENDS 4
+                //{
+                //    chatEmojiDisplay.transform.GetChild(i).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowRedEmoji(i));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                //}
+                    chatEmojiDisplay.transform.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowRedEmoji(0));
+                    chatEmojiDisplay.transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowRedEmoji(1));
+                    chatEmojiDisplay.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowRedEmoji(2));
+                    chatEmojiDisplay.transform.GetChild(3).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowRedEmoji(3));
+                    break;
+
+                case 1:
+                    chatEmojiDisplay.transform.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowBlueEmoji(0));
+                    chatEmojiDisplay.transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowBlueEmoji(1));
+                    chatEmojiDisplay.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowBlueEmoji(2));
+                    chatEmojiDisplay.transform.GetChild(3).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowBlueEmoji(3));
+                    break;
+                case 2:
+                    chatEmojiDisplay.transform.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowYellowEmoji(0));
+                    chatEmojiDisplay.transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowYellowEmoji(1));
+                    chatEmojiDisplay.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowYellowEmoji(2));
+                    chatEmojiDisplay.transform.GetChild(3).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowYellowEmoji(3));
+                    break;
+                case 3:
+                    chatEmojiDisplay.transform.GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowGreenEmoji(0));
+                    chatEmojiDisplay.transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowGreenEmoji(1));
+                    chatEmojiDisplay.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowGreenEmoji(2));
+                    chatEmojiDisplay.transform.GetChild(3).gameObject.GetComponent<Button>().onClick.AddListener(() => ShowGreenEmoji(3));
+                    break;
+                default:
+                    break;
+            }
     }
 
     void RegisterEvents()
     {
-        chatEmojiIndex.OnValueChanged += UpdateChatIcon;
+        chatEmojiIndex.OnValueChanged += UpdateRedChatIconOnOthers;
+        chatEmojiIndex1.OnValueChanged += UpdateBlueChatIconOnOthers;
+        chatEmojiIndex2.OnValueChanged += UpdateYellowChatIconOnOthers;
+        chatEmojiIndex3.OnValueChanged += UpdateGreenChatIconOnOthers;
     }
 
     void UnregisterEvents()
     {
-        chatEmojiIndex.OnValueChanged -= UpdateChatIcon;
+        chatEmojiIndex.OnValueChanged -= UpdateRedChatIconOnOthers;
+        chatEmojiIndex1.OnValueChanged -= UpdateBlueChatIconOnOthers;
+        chatEmojiIndex2.OnValueChanged -= UpdateYellowChatIconOnOthers;
+        chatEmojiIndex3.OnValueChanged -= UpdateGreenChatIconOnOthers;
     }
 
     void OnChatButtonPressed()
@@ -71,34 +104,150 @@ public class ChatSystem : NetworkBehaviour
         isChatOppened = true;
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    void ShowEmojiToOtherPlayersServerRpc()
+    void ShowRedEmoji(int emojiIndex)
     {
-        chatEmojiIndex.Value = Random.Range(1,5);
+        Debug.Log(emojiIndex);
+        //Update emoji on self
+        allChatDisplayIcons[0].gameObject.SetActive(true);
+        allChatDisplayIcons[0].sprite = emojis[emojiIndex];
+
+        //Update emoji on others
+        UpdateEmojiInArrayServerRpc(0, emojiIndex);
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject)); // Start the new coroutine
     }
 
-    void UpdateChatIcon(int prev, int newValue)
+    void ShowBlueEmoji(int emojiIndex)
     {
-        //Azuriraj tom klijentu prvi element niza
+        allChatDisplayIcons[0].gameObject.SetActive(true);
+        allChatDisplayIcons[0].sprite = emojis[emojiIndex];
 
+        UpdateEmojiInArrayServerRpc(1, emojiIndex);
 
-        //Azuriraj ostalim klijentima ono gde se on nalazi
-        for (int i = 0; i < 4; i++)
+        if (timerCoroutine != null)
         {
-            //allChatDisplayIcons[NetworkManager.Singleton.LocalClientId].color = Color.red;
-            //update player 0 (RED) chat icon to other clients
-            if ((i > 0 && i < 4) && (int)NetworkManager.Singleton.LocalClientId == i)
-            {
-                Debug.Log(i);
-                allChatDisplayIcons[1].color = Color.red;
-            }
-
-            //update other clients chat icon on player 0 (RED)
-            //if ((int)NetworkManager.Singleton.LocalClientId == 0 && i < 4)
-            //{
-            //allChatDisplayIcons[(int)currentTurn.Value].color = Color.red;
-            //}
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
         }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject)); // Start the new coroutine
+
+        //StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject));
+    }
+    void ShowYellowEmoji(int emojiIndex)
+    {
+        allChatDisplayIcons[0].gameObject.SetActive(true);
+        allChatDisplayIcons[0].sprite = emojis[emojiIndex];
+
+        UpdateEmojiInArrayServerRpc(2, emojiIndex);
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject)); // Start the new coroutine
+
+        //StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject));
+    }
+    void ShowGreenEmoji(int emojiIndex)
+    {
+        allChatDisplayIcons[0].gameObject.SetActive(true);
+        allChatDisplayIcons[0].sprite = emojis[emojiIndex];
+
+        UpdateEmojiInArrayServerRpc(3, emojiIndex);
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject)); // Start the new coroutine
+
+        //StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[0].gameObject));
+    }
+
+    [ServerRpc(RequireOwnership =false)]
+    void UpdateEmojiInArrayServerRpc(int playerIndex, int emojiIndex)
+    {
+        switch (playerIndex)
+        {
+            case 0:
+                chatEmojiIndex.Value = emojiIndex;
+                break;
+            case 1:
+                chatEmojiIndex1.Value = emojiIndex;
+                break;
+            case 2:
+                chatEmojiIndex2.Value = emojiIndex;
+                break;
+            case 3:
+                chatEmojiIndex3.Value = emojiIndex;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void UpdateRedChatIconOnOthers(int prev, int newValue)
+    {
+        if(NetworkManager.LocalClientId == 0) { return; }
+
+        allChatDisplayIcons[NetworkManager.Singleton.LocalClientId].gameObject.SetActive(true);
+        allChatDisplayIcons[NetworkManager.Singleton.LocalClientId].sprite = emojis[newValue];
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[NetworkManager.Singleton.LocalClientId].gameObject)); // Start the new coroutine
+
+        //StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[NetworkManager.Singleton.LocalClientId].gameObject));
+    }
+
+    IEnumerator TurnOffAfterDelay(GameObject icon)
+    {
+        yield return new WaitForSeconds(5); // Wait for the specified time
+        icon.SetActive(false);
+    }
+
+    void UpdateBlueChatIconOnOthers(int prev, int newValue)
+    {
+        if (NetworkManager.LocalClientId == 1) { return; }
+
+        allChatDisplayIcons[1].gameObject.SetActive(true);
+        allChatDisplayIcons[1].sprite = emojis[newValue];
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[1].gameObject)); // Start the new coroutine
+    }
+    void UpdateYellowChatIconOnOthers(int prev, int newValue)
+    {
+        if (NetworkManager.LocalClientId == 2) { return; }
+
+        allChatDisplayIcons[2].gameObject.SetActive(true);
+        allChatDisplayIcons[2].sprite = emojis[newValue];
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[2].gameObject)); // Start the new coroutine
+    }
+    void UpdateGreenChatIconOnOthers(int prev, int newValue)
+    {
+        if (NetworkManager.LocalClientId == 3) { return; }
+
+        allChatDisplayIcons[3].gameObject.SetActive(true);
+        allChatDisplayIcons[3].sprite = emojis[newValue];
+
+        if (timerCoroutine != null)
+        {
+            StopCoroutine(timerCoroutine); // Stop the old coroutine
+        }
+        timerCoroutine = StartCoroutine(TurnOffAfterDelay(allChatDisplayIcons[3].gameObject)); // Start the new coroutine
     }
 
     private void OnDestroy()
