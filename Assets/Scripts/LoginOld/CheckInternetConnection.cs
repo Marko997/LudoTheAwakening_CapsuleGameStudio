5b9f948cfb3846c618fc897190c0b6bc6751083e
MilosJovanovicRT-5218 <54739550+MilosJovanovicRT-5218@users.noreply.github.com>
2023-04-03 14:44:41 +0200

    Added:LoginSceneUI,FixCheckInternetConnection,GameSceneEndScreen

--- Assets/Scripts/LoginOld/CheckInternetConnection.cs
+++ Assets/Scripts/LoginOld/CheckInternetConnection.cs
@@ -6,29 +6,43 @@ using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 public class CheckInternetConnection : MonoBehaviour
 {
-    public Canvas InternetCanvas;
-    public GameObject InternetImage;
-    
+    //////public Canvas InternetCanvas;
+    //////public GameObject InternetImage;
+
+    //////public void Update()
+    //////{
+    //////    //StartCoroutine(CheckInternet());
+    //////    if (Application.internetReachability == NetworkReachability.NotReachable)
+    //////    {
+    //////        InternetImage.SetActive(true);
+    //////        InternetCanvas.enabled = true;
+    //////        AudioListener.volume = 0;
+    //////    }
+    //////    else
+    //////    {
+    //////        InternetImage.SetActive(false);
+    //////        InternetCanvas.enabled = false;
+    //////        AudioListener.volume = 1;
+    //////    }
+    //////}
+    ///
+    public CanvasGroup InternetCanvasGroup;
+
     public void Update()
     {
-        //StartCoroutine(CheckInternet());
         if (Application.internetReachability == NetworkReachability.NotReachable)
         {
-            InternetImage.SetActive(true);
-            InternetCanvas.enabled = true;
+            InternetCanvasGroup.alpha = 1;
+            InternetCanvasGroup.interactable = true;
+            InternetCanvasGroup.blocksRaycasts = true;
             AudioListener.volume = 0;
         }
         else
         {
-            InternetImage.SetActive(false);
-            InternetCanvas.enabled = false;
+            InternetCanvasGroup.alpha = 0;
+            InternetCanvasGroup.interactable = false;
+            InternetCanvasGroup.blocksRaycasts = false;
             AudioListener.volume = 1;
         }
     }
-    ////Ovo nam pravi problem sa kanvasom
-    //public void TryAgain()
-    //{
-    //    //SceneManager.LoadScene("LogingScene");
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
 }
