5d1d3a26e50b52faaf5f530c98ffd6209cbd525b
MilosJovanovicRT-5218 <54739550+MilosJovanovicRT-5218@users.noreply.github.com>
2023-04-05 21:20:50 +0200

    Added:LoadingText in LoadingScene

--- Assets/Scripts/Loading/LoadingAnimationController.cs
+++ Assets/Scripts/Loading/LoadingAnimationController.cs
@@ -8,8 +8,19 @@ public class LoadingAnimationController : MonoBehaviour
 {
     //////public GameObject loadingCanvas;
     //////public GameObject logoCanvas;
-
+    ////////public float sliderAnimationSpeed = 0.5f;
+    ////////public float sliderMoveDelay = 0.1f;
+    //////public float sliderAnimationSpeed;
+    //////public float sliderMoveDelay;
     //////public PlayFabAuthService AuthService = PlayFabAuthService.Instance;
+
+
+    //////private void Awake()
+    //////{
+    //////    sliderAnimationSpeed = Random.Range(0.3f, 0.8f);
+    //////    sliderMoveDelay = Random.Range(0.05f, 0.3f);
+    //////}
+
     //////private void Start()
     //////{
     //////    StartCoroutine(ChangeCanvas());
@@ -21,7 +32,7 @@ public class LoadingAnimationController : MonoBehaviour
 
     //////    Debug.Log(PlayerPrefs.GetInt("PlayFabLoginRemember"));
 
-    //////    yield return new WaitForSeconds(4);
+    //////    yield return new WaitForSeconds(0.5f);
 
     //////    if (PlayerPrefs.GetInt("PlayFabLoginRemember") == 0) // player not remembered
     //////    {
@@ -29,7 +40,37 @@ public class LoadingAnimationController : MonoBehaviour
     //////    }
     //////    else
     //////    {
-    //////        SceneManager.LoadScene("MainScene");
+    //////        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainScene");
+    //////        asyncLoad.allowSceneActivation = false;
+
+    //////        Slider loadingSlider = loadingCanvas.GetComponentInChildren<Slider>();
+    //////        loadingSlider.value = 0.1f;
+
+    //////        // Start slider animation
+    //////        float startTime = Time.time;
+    //////        while (Time.time - startTime < sliderMoveDelay)
+    //////        {
+    //////            loadingSlider.value = Mathf.Lerp(0.1f, 0.5f, (Time.time - startTime) / sliderMoveDelay);
+    //////            yield return null;
+    //////        }
+
+    //////        while (!asyncLoad.isDone)
+    //////        {
+    //////            float targetValue = asyncLoad.progress;
+    //////            while (loadingSlider.value < targetValue)
+    //////            {
+    //////                loadingSlider.value += Time.deltaTime * sliderAnimationSpeed;
+    //////                yield return null;
+    //////            }
+
+    //////            if (asyncLoad.progress >= 0.9f && loadingSlider.value >= 0.9f)
+    //////            {
+    //////                loadingSlider.value = 1f;
+    //////                asyncLoad.allowSceneActivation = true;
+    //////            }
+
+    //////            yield return null;
+    //////        }
     //////    }
     //////}
 
@@ -39,17 +80,96 @@ public class LoadingAnimationController : MonoBehaviour
 
     //////    loadingCanvas.SetActive(true);
 
-    //////    StartCoroutine(ChangeScene());
+    //////    yield return StartCoroutine(ChangeScene());
     //////}
+
+    ////////public GameObject loadingCanvas;
+    ////////public GameObject logoCanvas;
+    ////////public float sliderAnimationSpeed;
+    ////////public float sliderMoveDelay;
+    ////////public PlayFabAuthService AuthService = PlayFabAuthService.Instance;
+    ////////public Text procentLoading;
+
+    ////////private void Awake()
+    ////////{
+    ////////    sliderAnimationSpeed = Random.Range(0.3f, 0.8f);
+    ////////    sliderMoveDelay = Random.Range(0.05f, 0.3f);
+    ////////}
+
+    ////////private void Start()
+    ////////{
+    ////////    StartCoroutine(ChangeCanvas());
+    ////////}
+
+    ////////private IEnumerator ChangeScene()
+    ////////{
+    ////////    AuthService.Authenticate(Authtypes.Silent);
+
+    ////////    Debug.Log(PlayerPrefs.GetInt("PlayFabLoginRemember"));
+
+    ////////    yield return new WaitForSeconds(0.5f);
+
+    ////////    if (PlayerPrefs.GetInt("PlayFabLoginRemember") == 0) // player not remembered
+    ////////    {
+    ////////        SceneManager.LoadScene("LoginScene");
+    ////////    }
+    ////////    else
+    ////////    {
+    ////////        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainScene");
+    ////////        asyncLoad.allowSceneActivation = false;
+
+    ////////        Slider loadingSlider = loadingCanvas.GetComponentInChildren<Slider>();
+    ////////        loadingSlider.value = 0.1f;
+
+    ////////        // Start slider animation
+    ////////        float startTime = Time.time;
+    ////////        while (Time.time - startTime < sliderMoveDelay)
+    ////////        {
+    ////////            loadingSlider.value = Mathf.Lerp(0.1f, 0.5f, (Time.time - startTime) / sliderMoveDelay);
+    ////////            procentLoading.text = ((int)(loadingSlider.value * 100)).ToString() + "%";
+    ////////            yield return null;
+    ////////        }
+
+    ////////        while (!asyncLoad.isDone)
+    ////////        {
+    ////////            float targetValue = asyncLoad.progress;
+    ////////            while (loadingSlider.value < targetValue)
+    ////////            {
+    ////////                loadingSlider.value += Time.deltaTime * sliderAnimationSpeed;
+    ////////                procentLoading.text = ((int)(loadingSlider.value * 100)).ToString() + "%";
+    ////////                yield return null;
+    ////////            }
+
+    ////////            if (asyncLoad.progress >= 0.9f && loadingSlider.value >= 0.9f)
+    ////////            {
+    ////////                loadingSlider.value = 1f;
+    ////////                procentLoading.text = "100%";
+    ////////                asyncLoad.allowSceneActivation = true;
+    ////////            }
+
+    ////////            yield return null;
+    ////////        }
+    ////////    }
+    ////////}
+
+    ////////private IEnumerator ChangeCanvas()
+    ////////{
+    ////////    yield return new WaitForSeconds(4);
+
+    ////////    loadingCanvas.SetActive(true);
+
+    ////////    yield return StartCoroutine(ChangeScene());
+    ////////}
+    ///
 
     public GameObject loadingCanvas;
     public GameObject logoCanvas;
-    //public float sliderAnimationSpeed = 0.5f;
-    //public float sliderMoveDelay = 0.1f;
     public float sliderAnimationSpeed;
     public float sliderMoveDelay;
     public PlayFabAuthService AuthService = PlayFabAuthService.Instance;
 
+    public Text loadingText;
+    private string[] randomLoadingPhrases = new string[] { "Rolling dice", "Polishing board", "Polishing weapons", "Logging in" };
 
     private void Awake()
     {
@@ -117,6 +237,27 @@ public class LoadingAnimationController : MonoBehaviour
         loadingCanvas.SetActive(true);
 
         yield return StartCoroutine(ChangeScene());
+    }
+
+    private IEnumerator UpdateLoadingText()
+    {
+        int phraseIndex = 0;
+        while (true)
+        {
+            loadingText.text = randomLoadingPhrases[phraseIndex] + "... " + (loadingCanvas.GetComponentInChildren<Slider>().value * 100).ToString("F0") + "%";
+            phraseIndex = (phraseIndex + 1) % randomLoadingPhrases.Length;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(UpdateLoadingText());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
 }
