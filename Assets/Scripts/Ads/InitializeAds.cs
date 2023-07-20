39030771e51d6df9c0b2b7968fd345899442c412
Marko997 <mrankovic.it@gmail.com>
2023-07-20 21:16:47 +0200

    Unity ads Interstitial added

---
+++ Assets/Scripts/Ads/InitializeAds.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAds : MonoBehaviour, IUnityAdsInitializationListener
+{
+    public string androidGameId;
+    public string iosGameId;
+
+    public bool isTestingMode = false;
+
+    string gameId;
+
+    void AdsInitialization()
+    {
+        gameId = androidGameId;
+        gameId = iosGameId;
+
+#if UNITY_IOS
+        gameId = iosGameId;
+#elif UNITY_ANDROID
+        gameId = androidGameId;
+#elif UNITY_EDITOR
+        gameId = androidGameId; //for testing
+#endif
+        if(!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(gameId,isTestingMode, this);
+        }
+
+    }
+
+    void IUnityAdsInitializationListener.OnInitializationComplete()
+    {
+        print("Ads init");
+    }
+
+    void IUnityAdsInitializationListener.OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        print("Ads failed");
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        AdsInitialization();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ads/LoadInterstitial.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class LoadInterstitial : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    public string androidAdUnityId;
+    public string iosAdUnityId;
+
+    string adUnitId;
+
+    private void Awake()
+    {
+#if UNITY_IOS
+        adUnitId = iosAdUnityId;
+#elif UNITY_ANDROID
+        adUnitId = androidAdUnityId;
+#endif
+    }
+
+    public void LoadInterstitialAd()
+    {
+        print("Interstitial add loaded");
+        Advertisement.Load(adUnitId, this);
+    }
+
+    public void ShowAd()
+    {
+        print("Showing ad");
+        Advertisement.Show(adUnitId,this);
+    }
+
+
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        print("Interstitial loaded");
+        ShowAd();
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        print("Interstitial failed to load");
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        print("Interstitial clicked");
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        print("Interstitial completed");
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        print("Interstitial show failed");
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        print("Interstitial show start");
+    }
+}
