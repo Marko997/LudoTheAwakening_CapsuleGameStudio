f15eb3d56fe02eb0e461d38488d88c3a90ff9dbb
Marko997 <mrankovic.it@gmail.com>
2023-07-29 08:42:13 +0200

    Fixed login insert name

---
+++ Assets/Scripts/PlayFab/PlayFabManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.EconomyModels;
+using TMPro;
+using UnityEngine;
+
+public class PlayFabManager : MonoBehaviour
+{
+    #region Singleton
+    private static PlayFabManager instance;
+    public static PlayFabManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<PlayFabManager>();
+            }
+            if (instance == null)
+            {
+                Debug.Log("There is no PlayFab manager!");
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public TMP_Text coinText;
+    public TMP_Text rubyText;
+
+    public string rubyId = "d6f4ae84-dd6e-4f9e-b38d-0249fbd3d1a4";
+    public string coinId = "f7c88f86-c896-42a2-96fa-ec9f0cab12c9";
+
+    public void AddVirtualCurrency(string id, int amount)
+    {
+        PlayFabEconomyAPI.AddInventoryItems(new AddInventoryItemsRequest
+        {
+            Item = new InventoryItemReference
+            {
+                Id = id
+            },
+            Amount = amount
+        }, OnAddCurrencySuccess, OnError);
+    }
+
+    void OnAddCurrencySuccess(AddInventoryItemsResponse response)
+    {
+        Debug.Log($"Added {response.TransactionIds[0]} rubies");
+        GetVirtualCurrencies();
+
+    }
+
+    public void GetVirtualCurrencies()
+    {
+        PlayFabEconomyAPI.GetInventoryItems(new GetInventoryItemsRequest
+        {
+            Filter = "type eq 'currency'"
+
+        }, OnGetUserInventorySuccess, OnError);
+    }
+
+    void OnGetUserInventorySuccess(GetInventoryItemsResponse response)
+    {
+        for (int i = 0; i < response.Items.Count; i++)
+        {
+            if (response.Items[i].Id == "d6f4ae84-dd6e-4f9e-b38d-0249fbd3d1a4") //ruby
+            {
+                int rubies = response.Items[i].Amount;
+                rubyText.text = $"{rubies}";
+            }
+            if (response.Items[i].Id == "f7c88f86-c896-42a2-96fa-ec9f0cab12c9") // coin
+            {
+                int coins = response.Items[i].Amount;
+                coinText.text = $"{coins}";
+            }
+        }
+    }
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error " + error.ErrorMessage);
+    }
+}
---
+++ Assets/Scripts/PlayFab/Scripts/GetPlayerInfo.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.EconomyModels;
+using TMPro; 
+
+public class GetPlayerInfo : MonoBehaviour
+{
+    public TMP_Text displayNameText;
+    public TMP_Text playerIdText;
+
+    public TMP_Text errorChangeName;
+
+
+
+    //bool isDataRecieved;
+
+    #region Singleton
+    private static GetPlayerInfo instance;
+    public static GetPlayerInfo Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GetPlayerInfo>();
+            }
+            if (instance == null)
+            {
+                Debug.Log("There is no PlayFab manager!");
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    //private void Awake()
+    //{
+    //    GetAccountInfoRequest request = new GetAccountInfoRequest();
+    //    PlayFabClientAPI.GetAccountInfo(request, OnGetAccountInfoSuccess, OnGetAccountInfoError);
+
+    //}
+
+    public void Start()
+    {
+        //if (isDataRecieved) { return; }
+        GetAccountInfoRequest request = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(request, OnGetAccountInfoSuccess, OnGetAccountInfoError);
+    }
+
+    void OnGetAccountInfoSuccess(GetAccountInfoResult result)
+    {
+        PlayFabManager.Instance.GetVirtualCurrencies();
+        //PlayFabManager.Instance.AddVirtualCurrency("f7c88f86-c896-42a2-96fa-ec9f0cab12c9",10);
+        //PlayFabManager.Instance.AddVirtualCurrency("d6f4ae84-dd6e-4f9e-b38d-0249fbd3d1a4", 10);
+        string displayName = result.AccountInfo.TitleInfo.DisplayName;
+        string playerId = result.AccountInfo.PlayFabId;
+
+        PlayerPrefs.SetString("NAME", displayName);
+
+        displayNameText.text = "Name: " + displayName;
+        playerIdText.text = "ID: " + playerId;
+    }
+
+    void OnGetAccountInfoError(PlayFabError error)
+    {
+        Debug.Log("Error: " + error.ErrorMessage);//getting account info
+        errorChangeName.text = error.ErrorMessage;
+    }
+
+    public void OnChangeNameClicked()
+    { 
+        displayNameText.text = "Name: " + PlayerPrefs.GetString("NAME");
+        Debug.Log(PlayerPrefs.GetString("NAME"));
+    }
+}
